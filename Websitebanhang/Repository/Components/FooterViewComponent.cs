922ccf646377440d4a94918455790cf003e52596
Thinh02vq <thinh02vq@gmail.com>
2026-05-19 17:22:42 +0700

    footer

---
+++ Websitebanhang/Repository/Components/FooterViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Websitebanhang.Repository.Components
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+        public FooterViewComponent(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _dataContext.Contacts.FirstOrDefaultAsync());
+
+    }
+}
